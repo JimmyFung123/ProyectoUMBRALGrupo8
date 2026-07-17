d4b5173908f22b36d55793d47810fa033752a3d9
JimmyFung123 <jimmyfung16@gmail.com>
2026-07-17 13:17:01 -0400

    [Added] Clase base AggregateRoot con colección de domain events en los 4 microservicios

---
+++ UMBRAL_Back-end/SessionService/Domain/Common/AggregateRoot.cs
@@ -0,0 +1,12 @@
+namespace SessionService.Domain.Common;
+
+public abstract class AggregateRoot
+{
+    private readonly List<IDomainEvent> _domainEvents = new();
+
+    public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    protected void AddDomainEvent(IDomainEvent domainEvent) => _domainEvents.Add(domainEvent);
+
+    public void ClearDomainEvents() => _domainEvents.Clear();
+}
